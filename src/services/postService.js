43b2db1e1bcb54d6f5a885d4cfb4eb0ae5ce097d
sw1104 <sangw1104@gmail.com>
2022-10-11 16:33:26 +0900

    Feat: 특정 검색어로 채용공고 조회하기 API (#8)

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,3 +1,4 @@
+const BaseError = require("../middlewares/baseError");
 const postDao = require("../models/postDao");
 
 const postRegistration = async (companyId, position, compensation, content, technologyStack) => {
@@ -16,9 +17,17 @@ const getPost = async () => {
     return await postDao.getPost();
 }
 
+const getSearchPost = async (search) => {
+    const result = await postDao.getSearchPost(search);
+    if (result.length === 0) throw new BaseError("NO RESULTS WERE FOUND FOR YOUR SEARCH.", 400);
+
+    return result;
+}
+
 module.exports = {
     postRegistration,
     postEdit,
     postDelete,
-    getPost
+    getPost,
+    getSearchPost
 }

6e977b4c3928865a995acfe69061cdbfba0107ad
sw1104 <sangw1104@gmail.com>
2022-10-11 13:28:26 +0900

    Feat: 기업이 등록한 채용공고 수정 API (#4)

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,15 +1,26 @@
 const postService = require("../services/postService");
-const baseError = require("../middlewares/baseError")
+const baseError = require("../middlewares/baseError");
 
 const postRegistration = async (req, res) => {
     const { companyId, position, compensation, content, technologyStack } = req.body;
 
-    if (!companyId || !position || !compensation || !content || !technologyStack) throw new baseError("KEY ERROR", 400);
+    if (!companyId || !position || !compensation || !content || !technologyStack) throw new baseError("KEY ERROR", 400)
 
     await postService.postRegistration(companyId, position, compensation, content, technologyStack);
     res.status(201).json({ message: "Job posting completed" });
 }
 
+const postEdit = async (req, res) => {
+    const { companyId, postId } = req.params;
+    const { position, compensation, content, technologyStack } = req.body;
+
+    if (!companyId || !postId) throw new baseError("KEY ERROR", 400)
+
+    await postService.postEdit(companyId, postId, position, compensation, content, technologyStack)
+    res.status(201).json({ messgae: "Post edited" })
+}
+
 module.exports = {
-    postRegistration
+    postRegistration,
+    postEdit
 }
--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const postController = require("../controllers/postController");
 router.post("", postController.postRegistration);
+router.patch("/:companyId/:postId", postController.postEdit);
 
 module.exports = {
     router
